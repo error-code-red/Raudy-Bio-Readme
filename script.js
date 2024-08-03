0bb42e23e803ced42ec77867a3d17ad52604a875
error-code-red <149626268+error-code-red@users.noreply.github.com>
2024-08-03 20:52:48 +0000

    madlabs completion

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let myName = "TimBillBob"
 console.log(myName)
 
+let adj = "forgetful"
+
 let num = 10
 console.log(num)
 
 let express = 2 + 5
 console.log(express)
 
-let needsShop = true
-console.log(needsShop)
+let hasMoney = true
+console.log(hasMoney)
 
-let item = prompt("What would you like to buy?")
+let item = prompt("What should TimBillBob buy?")
 console.log(item)
+
+console.log( myName + " needs to buy something but they are " + adj + " they need " + num + " of something since they already have " + express + " They bought " + item + " Do they have enough money? That is " + hasMoney )
